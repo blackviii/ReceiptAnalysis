f8d53fe6dbba6b71bdad7a151756a33517cb1a0b
MikeZhao <chengqian.zhao@hotmail.com>
2024-05-17 17:36:26 -0700

    app.py file to test app display

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_time():
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    return f"Current Time: {current_time}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
--- testservce.py
+++ testservce.py
@@ -1,9 +1,3 @@
 #把当前时间打印出来精确到秒
 import time
 print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
